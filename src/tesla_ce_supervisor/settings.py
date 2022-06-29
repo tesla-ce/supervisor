b9a21640b5ee4eff8a4f2a5d91ea21c7aa77f34c
Xavier Baró <xavierbaro@gmail.com>
2022-06-29 11:19:55 +0200

    Fixed static file generation on Dockercompose

--- src/tesla_ce_supervisor/settings.py
+++ src/tesla_ce_supervisor/settings.py
@@ -188,7 +188,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 STATIC_URL = 'supervisor/static/'
-STATIC_ROOT = ''
+STATIC_ROOT = os.getenv('STATICS_PATH', '')
 
 
 # Default primary key field type
