a9ac20074084ed78b7a7907f1784bfa84539abd8
Xavier Baró <xavierbaro@gmail.com>
2022-07-05 13:54:40 +0200

    Update base.py

--- src/tesla_ce_supervisor/lib/deploy/base.py
+++ src/tesla_ce_supervisor/lib/deploy/base.py
@@ -1,12 +1,26 @@
 import abc
 import typing
 
+from django.contrib.auth.models import User
+
+from .exceptions import TeslaDeployException
 from ..setup_options import SetupOptions
 from ..tesla.conf import Config
 from ..models.check import ServiceDeploymentInformation, ConnectionStatus
 
+ModuleCode = typing.Union[
+    typing.Literal['LB'],
+    typing.Literal['DATABASE'],
+    typing.Literal['MINIO'],
+    typing.Literal['RABBITMQ'],
+    typing.Literal['REDIS'],
+    typing.Literal['VAULT'],
+    typing.Literal['SUPERVISOR'],
+]
 
 NOT_IMPLEMENTED_MESSAGE = 'Method not implemented'
+INVALID_MODULE_MESSAGE = 'Invalid module name {}. ' \
+                         'Valid names are: lb, minio, database, rabbitmq, redis, supervisor, vault.'
 
 
 class BaseDeploy(abc.ABC):
@@ -227,3 +241,115 @@ class BaseDeploy(abc.ABC):
             Get the deployment information for TeSLA CE Supervisor
         """
         raise NotImplementedError(NOT_IMPLEMENTED_MESSAGE)
+
+    def genereate_deployment_credentials(self, module: ModuleCode):
+        """
+            Generate required credentials for a given module
+            :param module: Name of the module
+        """
+        if module.upper() == "LB":
+            # No configuration needed
+            pass
+        elif module.upper() == "DATABASE":
+            pass
+        elif module.upper() == "REDIS":
+            pass
+        elif module.upper() == "MINIO":
+            pass
+        elif module.upper() == "RABBITMQ":
+            pass
+        elif module.upper() == "VAULT":
+            # No configuration needed
+            pass
+        elif module.upper() == "SUPERVISOR":
+            # Check if admin password exists or must be created
+            if self._config.get('SUPERVISOR_ADMIN_PASSWORD') is None:
+                self._config.set('SUPERVISOR_ADMIN_PASSWORD', User.objects.make_random_password())
+
+    def get_status(self, module: ModuleCode):
+        """
+            Get module status
+            :param module: Name of the module
+        """
+        if module.upper() == "LB":
+            return self.get_lb_status()
+        elif module.upper() == "DATABASE":
+            return self.get_database_status()
+        elif module.upper() == "REDIS":
+            return self.get_redis_status()
+        elif module.upper() == "MINIO":
+            return self.get_minio_status()
+        elif module.upper() == "RABBITMQ":
+            return self.get_rabbitmq_status()
+        elif module.upper() == "VAULT":
+            return self.get_vault_status()
+        elif module.upper() == "SUPERVISOR":
+            return self.get_supervisor_status()
+
+        raise TeslaDeployException(INVALID_MODULE_MESSAGE.format(module))
+
+    def get_script(self, module: ModuleCode):
+        """
+            Get deployment script
+            :param module: Name of the module
+        """
+        if module.upper() == "LB":
+            return self.get_lb_script()
+        elif module.upper() == "DATABASE":
+            return self.get_database_script()
+        elif module.upper() == "REDIS":
+            return self.get_redis_script()
+        elif module.upper() == "MINIO":
+            return self.get_minio_script()
+        elif module.upper() == "RABBITMQ":
+            return self.get_rabbitmq_script()
+        elif module.upper() == "VAULT":
+            return self.get_vault_script()
+        elif module.upper() == "SUPERVISOR":
+            return self.get_supervisor_script()
+
+        raise TeslaDeployException(INVALID_MODULE_MESSAGE.format(module))
+
+    def deploy(self, module: ModuleCode):
+        """
+            Deploy a module
+            :param module: Name of the module
+        """
+        if module.upper() == "LB":
+            return self.deploy_lb()
+        elif module.upper() == "DATABASE":
+            return self.deploy_database()
+        elif module.upper() == "REDIS":
+            return self.deploy_redis()
+        elif module.upper() == "MINIO":
+            return self.deploy_minio()
+        elif module.upper() == "RABBITMQ":
+            return self.deploy_rabbitmq()
+        elif module.upper() == "VAULT":
+            return self.deploy_vault()
+        elif module.upper() == "SUPERVISOR":
+            return self.deploy_supervisor()
+
+        raise TeslaDeployException(INVALID_MODULE_MESSAGE.format(module))
+
+    def remove(self, module: ModuleCode):
+        """
+            Remove deployed module
+            :param module: Name of the module
+        """
+        if module.upper() == "LB":
+            return self.remove_lb()
+        elif module.upper() == "DATABASE":
+            return self.remove_database()
+        elif module.upper() == "REDIS":
+            return self.remove_redis()
+        elif module.upper() == "MINIO":
+            return self.remove_minio()
+        elif module.upper() == "RABBITMQ":
+            return self.remove_rabbitmq()
+        elif module.upper() == "VAULT":
+            return self.remove_vault()
+        elif module.upper() == "SUPERVISOR":
+            return self.remove_supervisor()
+
+        raise TeslaDeployException(INVALID_MODULE_MESSAGE.format(module))
