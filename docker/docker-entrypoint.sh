b0ea125c213828cd3e14da02098f067f3cb26d9c
Roger <roger@rcod.net>
2023-05-25 11:14:36 +0200

    fix db_password

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -2,10 +2,11 @@
 
 set -e
 
+if [ ! -f $POSTGRES_PASSWORD_FILE ]; then
+  cat /proc/sys/kernel/random/uuid | sed 's/[-]//g' > $POSTGRES_PASSWORD_FILE
+fi
+
 if [ "$SETUP_MODE" != 'SETUP' ]; then
-  if [ ! -f $POSTGRES_PASSWORD_FILE ]; then
-    cat /proc/sys/kernel/random/uuid | sed 's/[-]//g' > $POSTGRES_PASSWORD_FILE
-  fi
   /bin/postgres_docker-entrypoint.sh postgres &
   /bin/wait_for_it.sh "localhost:5432" -- echo "Database is up"
   SETUP_MODE=BUILD /venv/bin/python manage.py migrate
